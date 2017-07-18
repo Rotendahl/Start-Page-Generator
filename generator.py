e7bd6f4c18f4bf2c603aea7048de3e4f48b89558
Benjamin Rotendahl <Benjamin@Rotendahl.dk>
2017-07-18 12:04:48 +0200

    Added generator and template

---
+++ generator.py
@@ -0,0 +1,34 @@
+from jinja2 import Template
+
+
+def readLinks():
+    links = []
+    with open('links.txt', 'r') as linksFile:
+        for link in linksFile.read().splitlines():
+            if not '*' in link:
+                continue
+            link = link.replace(' ', '').replace('*', '')
+
+            fields = {'name' : '', 'url' : '', 'logo' : ''}
+
+            if "->" in link:
+                link = link.split('->')
+                fields['name'] = link[0]
+                link = link[1]
+            if "<-" in link:
+                link = link.split('<-')
+                fields['logo'] = link[1]
+                link = link[0]
+
+            for field in fields:
+                fields[field] = link if fields[field] == '' else fields[field]
+
+            links.append(fields)
+            print(fields)
+    return links
+
+
+html = open('template.html', 'r').read()
+template = Template(html)
+with open('start.html', 'w') as out:
+    out.write(template.render({'links' : readLinks()}))
